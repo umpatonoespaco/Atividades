91209bd2526382f6c38caa43d47558d7d9836f4e
umpatonoespaco <89821565+umpatonoespaco@users.noreply.github.com>
2024-09-10 16:07:50 -0300

    Add files via upload

---
+++ Peripecias/A+B.py
@@ -0,0 +1,8 @@
+A = int(input('digite o valor de A\n'))
+B = int(input('digite o valor de B\n'))
+C = int(input('digite o valor de C\n'))
+
+if A + B <= C:
+    print('A + B e menor que C')
+else:
+    print('A + B e maior que C')
---
+++ Peripecias/Pitagoras.py
@@ -0,0 +1,6 @@
+cateto1 = int(input('Insira o valor do primeiro cateto\n'))
+cateto2 = int(input('Insira o valor do segundo cateto\n'))
+
+hipotenusa = (cateto1 ** 2 + cateto2 ** 2) ** 0.5
+
+print(f'A hipotenusa do triangulo e {hipotenusa}')
