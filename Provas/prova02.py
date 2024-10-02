c705ca5b3a7f2f1c4e63884085cadb7190033481
umpatonoespaco <89821565+umpatonoespaco@users.noreply.github.com>
2024-10-02 16:56:25 -0300

    Add files via upload

---
+++ Provas/prova02.py
@@ -0,0 +1,11 @@
+numero = int(input('Insira um numero\n'))
+
+if numero == 0:
+    print(f'{numero} e zero')
+
+elif numero >= 0:
+    print(f'O {numero} e positivo')
+
+elif numero <= 0:
+    print(f'{numero} e negativo')
+
